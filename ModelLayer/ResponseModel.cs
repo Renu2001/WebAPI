553d53117eea2ad737a4b83a990f3beed06983f6
Renuka <renu.bagave1901@gmail.com>
2024-06-05 10:51:06 +0530

    [Renuka] Add : Added Response model class to pass message, sucess status and data

---
+++ ModelLayer/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class ResponseModel
+    {
+        public string Success {  get; set; }
+        public string Message { get; set; }
+        public object Data {  get; set; }
+
+    }
+}
--- Projectname/Controllers/UserController.cs
+++ Projectname/Controllers/UserController.cs
@@ -11,81 +11,113 @@ namespace Projectname.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserBL userBL;
+        public ResponseModel response;
 
         public UserController(IUserBL userBL)
         {
             this.userBL = userBL;
+            this.response = new ResponseModel();
         }
 
         [HttpPost]
-        public IActionResult Adduser(UserModel model)
+        public ResponseModel Adduser(UserModel model)
         {
             var result = userBL.Adduser(model);
 
             if (result != null)
             {
-                return Ok(result);
+                this.response.Success = "true";
+                this.response.Message = "User Added Successfully";
+                this.response.Data = result;
+                return response;
             }
             else
             {
-                return BadRequest("Something Went Wrong!!!");
+                this.response.Success = "false";
+                this.response.Message = "User Not Added";
+                this.response.Data = null;
+                return response;
             }
         }
 
 
         [HttpPut]
-        public IActionResult Updateuser(int id, UserModel model)
+        public ResponseModel Updateuser(int id, UserModel model)
         {
             var result = userBL.Updateuser(id, model);
             if (result != null)
             {
-                return Ok(result);
+                this.response.Success = "true";
+                this.response.Message = "User Updated Successfully";
+                this.response.Data = result;
+                return response;
             }
             else
             {
-                return BadRequest("Not Updated Successfully ");
+                this.response.Success = "false";
+                this.response.Message = "User Not Updated";
+                this.response.Data = null;
+                return response;
             }
         }
         [HttpDelete]
-        public IActionResult Deleteuser(int id)
+        public ResponseModel Deleteuser(int id)
         {
             var result = userBL.DeleteUser(id);
             if (result != null)
             {
-                return Ok(result);
+                this.response.Success = "true";
+                this.response.Message = "User Deleted Successfully";
+                this.response.Data = result;
+                return response;
             }
             else
             {
-                return BadRequest("Something went Wrong !!!");
+                this.response.Success = "false";
+                this.response.Message = "User Not Deleted";
+                this.response.Data = null;
+                return response;
             }
         }
         [HttpGet]
 
-        public IActionResult GetUsers()
+        public ResponseModel GetUsers()
         {
             var result = userBL.Getusers();
             if (result != null)
             {
-                return Ok(result);
+                this.response.Success = "true";
+                this.response.Message = "Users Found";
+                this.response.Data = result;
+                return response;
             }
             else
             {
-                return NotFound("User Not Found!!!");
+                this.response.Success = "false";
+                this.response.Message = "No User Found";
+                this.response.Data = null;
+                return response;
             }
         }
 
         [HttpGet]
         [Route("{name}")]
-        public IActionResult GetUser(string name)
+        public ResponseModel GetUser(string name)
         {
             var result = userBL.Getuser(name);
             if (result != null)
             {
-                return Ok(result);
+                this.response.Success = "true";
+                this.response.Message = "User Found";
+                this.response.Data = result;
+                return response;
             }
             else
             {
-                return NotFound("User Not Found!!!");
+                this.response.Success = "false";
+                this.response.Message = "No User Found";
+                this.response.Data = null;
+                return response;
             }
         }
 
